382e1618166d5b2f42d32ef3ebfacde959a9c0dc
UwanaAssam0 <assamuwana0@gmail.com>
2023-10-24 17:19:26 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,46 +1,28 @@
 #include "main.h"
-
 /**
- * rot13 - Encodes a string using rot13.
- * @str: The string to be encoded.
+ * rot13 - change letters to ROT13.
+ * @s: analized string.
  *
- * Return: A pointer to the encoded string.
+ * Return: String with all letters in ROT13 base.
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-		‘G', 'H', 'I', 'J', 'K', 'L',m’,
-		‘M', 'N', 'O', 'P', 'Q', 'R',
-		‘S', 'T', 'U', 'V', 'W', 'X',
-		‘Y', 'Z', 'a', 'b', 'c', 'd',
-		‘e', 'f', 'g', 'h', 'i', 'j',
-		‘k', 'l', 'm', 'n', 'o', 'p',
-		‘q', 'r', 's', 't', 'u', 'v',
-		‘w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		‘T', 'U', 'V', 'W', 'X', 'Y',
-		‘Z', 'A', 'B', 'C', 'D', 'E',
-		‘F', 'G', 'H', 'I', 'J', 'K',
-		‘L', 'M', 'n', 'o', 'p', 'q',
-		‘r', 's', 't', 'u', 'v', 'w',
-		‘x', 'y', 'z', 'a', 'b', 'c',
-		‘d', 'e', 'f', 'g', 'h', 'i',
-		‘j', 'k', 'l', 'm'};
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j;
 
-	while (str[indx1])
+	while (*(s + i) != '\0')
 	{
-		for (indx2 = 0; indx2 < 52; indx2++)
+		for (j = 0; j <= 51; j++)
 		{
-			if (str[indx1] == alphabet[indx2])
+			if (*(s + i) == a[j])
 			{
-				str[indx1] = rot13key[indx2];
+				*(s + i) = rot[j];
 				break;
 			}
 		}
-
-		indx1++;
+		i++;
 	}
-
-	return (str);
+	return (s);
 }
